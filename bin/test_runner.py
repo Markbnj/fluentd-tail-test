8408eb20c02c4b5eee535c677642bf42499c8b2b
Mark Betz <betz.mark@gmail.com>
2020-06-29 02:13:18 -0400

    Pass in the desired log output per sec rather than a specific write delay.

--- bin/test_runner.py
+++ bin/test_runner.py
@@ -7,7 +7,7 @@ from multiprocessing import Process
 import os
 
 
-def write_logs(run_for, write_delay, id, output_type, path):
+def write_logs(run_for, events_per_sec, id, output_type, path):
   logger = logging.getLogger(__name__)
   logger.setLevel(logging.DEBUG)
   handler = None
@@ -25,6 +25,7 @@ def write_logs(run_for, write_delay, id, output_type, path):
   handler.setFormatter(logging.Formatter('{"time":"%(asctime)s","log":"%(message)s"}'))
   logger.addHandler(handler)
 
+  write_delay = 1000000/events_per_sec
   start_s = time.time()
   line_count = 0
   while True:
@@ -38,10 +39,10 @@ def write_logs(run_for, write_delay, id, output_type, path):
   print(f'writer {os.getpid()} exiting; wrote {line_count} lines in {ran_for} seconds.')
 
 
-def run_test(num_writers, run_for, write_delay, output_type, path):
+def run_test(num_writers, run_for, events_per_sec, output_type, path):
   procs = []
   for i in range(num_writers):
-    p = Process(target=write_logs, args=(run_for,write_delay,i,output_type,path))
+    p = Process(target=write_logs, args=(run_for,events_per_sec,i,output_type,path))
     p.start()
     procs.append(p)
   while [p.exitcode for p in procs if p.exitcode is None]:
@@ -52,10 +53,10 @@ if __name__ == "__main__":
   parser = ArgumentParser(description='Run a log write test using multiple writers')
   parser.add_argument('-n', '--num-writers', type=int, required=True, help='The number of log writers to spawn.')
   parser.add_argument('-r', '--run-for', type=int, required=True, help='Running time of the test in seconds.')
-  parser.add_argument('-w', '--write-delay', type=int, required=True, help='Delay between writes in microseconds.')
+  parser.add_argument('-e', '--events-per-sec', type=int, required=True, help='Number of events to emit per second.')
   parser.add_argument('-o', '--output-type', required=True, help='Kind of log output file|network')
   parser.add_argument('-p', '--path', default='srclogs', help='Path where logs are written.')
 
   args = parser.parse_args()
   print(f'spawning log writers with args: {args}')
-  run_test(args.num_writers, args.run_for, args.write_delay, args.output_type, args.path )
+  run_test(args.num_writers, args.run_for, args.events_per_sec, args.output_type, args.path )
